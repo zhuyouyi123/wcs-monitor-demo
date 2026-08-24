3ffddf190420a3ab26991e1eb8bf342a717683de
zhuyouyi <zhuyouyi@joysuch.com>
2026-08-24 15:19:38 +0800

    add:初始化项目 当前可启动

---
+++ src/main/java/com/wcs/monitor/WcsMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.wcs.monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WcsMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WcsMonitorApplication.class, args);
+    }
+
+}
